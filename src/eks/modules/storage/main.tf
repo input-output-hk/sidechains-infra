a5042bfbd93b46e1e9e2881e65a2abc8a830607b
skylarsimoncelli <skylar.simoncelli@icloud.com>
2023-06-16 12:29:19 +0100

    feat: added client access vpn

--- src/eks/modules/storage/main.tf
+++ src/eks/modules/storage/main.tf
@@ -62,6 +62,11 @@ resource "aws_iam_role_policy_attachment" "this" {
 }
 
 
+####################################################################################
+####################################################################################
+####################################################################################
+
+# Kubernetes Configuration
 
 provider "aws" {
   region = "eu-central-1"
@@ -81,6 +86,10 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
 }
+
+####################################################################################
+####################################################################################
+####################################################################################
 
 # EFS Volume Configuration
 
@@ -115,6 +124,10 @@ resource "kubernetes_storage_class" "efs" {
   reclaim_policy  = "Retain"
   volume_binding_mode = "WaitForFirstConsumer"
 }
+
+####################################################################################
+####################################################################################
+####################################################################################
 
 # NLB Configuration
 
@@ -228,4 +241,65 @@ resource "aws_lb_listener" "eve" {
     target_group_arn = aws_lb_target_group.eve.arn
   }
 }
+
+####################################################################################
+####################################################################################
+####################################################################################
+
+# AWS Client VPN Endpoint
+
+resource "aws_ec2_client_vpn_endpoint" "vpn_endpoint" {
+  description            = "VPN access to services"
+  server_certificate_arn = aws_acm_certificate.cert.arn
+  client_cidr_block      = "172.20.0.0/16" # iog-sidechain-substrate-kubernetes cluster cidr
+
+  authentication_options {
+    type                       = "certificate-authentication"
+    root_certificate_chain_arn = aws_acm_certificate.root_cert.arn
+  }
+
+  connection_log_options {
+    enabled               = true
+    cloudwatch_log_group  = aws_cloudwatch_log_group.lg.name
+    cloudwatch_log_stream = aws_cloudwatch_log_stream.ls.name
+  }
+
+  dns_servers = ["8.8.8.8", "8.8.4.4"]
+
+  transport_protocol = "udp"
+}
+
+locals {
+  subnet_ids = ["subnet-0245772cf491e28da", "subnet-0dcd319e426813a6c", "subnet-034a9423a9ac17ed7"]
+}
+
+resource "aws_ec2_client_vpn_network_association" "subnet" {
+  for_each = toset(local.subnet_ids)
+
+  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.client_vpn_endpoint.id
+  subnet_id              = each.key
+}
+
+# Generate server certificate for VPN (You can use your own certificates too)
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "vpn.sc.iog.io" # Replace with your domain
+  validation_method = "DNS"
+}
+
+# Generate root certificate for VPN client authentication (You can use your own certificates too)
+resource "aws_acm_certificate" "root_cert" {
+  domain_name       = "vpn.sc.iog.io" # Replace with your domain
+  validation_method = "DNS"
+}
+
+# Set up a CloudWatch Log Group and Log Stream for VPN connection logging
+resource "aws_cloudwatch_log_group" "lg" {
+  name = "VPN-Connection-Logs"
+}
+
+resource "aws_cloudwatch_log_stream" "ls" {
+  name           = "VPN-Connection-Log-Stream"
+  log_group_name = aws_cloudwatch_log_group.lg.name
+}
+
 
