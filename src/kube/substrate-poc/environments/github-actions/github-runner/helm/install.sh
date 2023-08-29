d653f82d8ccc5e4172f9051cda8b925b8b17c8ea
Skylar Simoncelli <skylar@skylar.email>
2023-08-29 10:33:27 +0100

    fix: space

--- src/kube/substrate-poc/environments/github-actions/github-runner/helm/install.sh
+++ src/kube/substrate-poc/environments/github-actions/github-runner/helm/install.sh
@@ -1,4 +1,4 @@
 helm upgrade --install --namespace actions-runner-system --create-namespace\
   --set=authSecret.create=true\
-  --set=authSecret.github_token=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\
+  --set=authSecret.github_token=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 \
   --wait actions-runner-controller actions-runner-controller/actions-runner-controller
